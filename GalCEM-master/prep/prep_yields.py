ee6fa905e7681e1cc7b04497ae0b6e3ef96c1742
egjergo <eda.gjergo@gmail.com>
2021-11-16 19:08:08 +0800

    if you want to rename the headers

--- GalCEM-master/prep/prep_yields.py
+++ GalCEM-master/prep/prep_yields.py
@@ -4,9 +4,7 @@ import os.path
 # Run in the master directory
 
 def lc18_example(filename='tab_yieldstot_iso_exp_pd.dec', loc='input/yields/snii/lc18/tab_R', 
-                 outfilename='lc18_pandas.csv', split_size=12, columns=['elemSymb', 'elemZ',
-                  'elemA', 'initial', '013a000', '015a000', '020a000', '025a000', '030a000',
-                   '040a000', '060a000', '080a000', '120a000']):
+                 outfilename='lc18_pandas.csv', split_size=12):
     '''
     Example of how to melt a custom yield with consecutive even tables
     into a GalCEM-friendly input format.
@@ -21,18 +19,20 @@ def lc18_example(filename='tab_yieldstot_iso_exp_pd.dec', loc='input/yields/snii
         print(f'Table import. \n {df}')
         df = np.array_split(df, split_size) 
         print(f'Table split. \n {df}')
-        new_headers = [d.iloc[0].values for d in df]
-        f = [pd.DataFrame(d[1:]) for d in df]
-        for i,val in f:
-            val.columns = new_headers[i]
-
-        f.to_csv(output_name, header=True, index=False)
+        # If you want to rename the headers
+        #new_headers = [d.iloc[0].values for d in df]
+        #f = [pd.DataFrame(d[1:]) for d in df]
+        #for i, val in enumerate(f):
+        #    val.columns = new_headers[i]
+        #print(f'Table rename. \n {f}')
+        df = pd.DataFrame(df)
+        df.to_csv(output_name, header=True, index=False)
         print(f'Processed yields saved in {output_name}')
     return None
 
 def k10_example(filename=['Z0.0001.dat', 'Z0.008.dat', 'Z0.004.dat', 'Z0.02.dat'], 
                 loc='input/yields/lims/k10', outfilename='k10_pandas.csv',
-                columns=['Mini','Zini','Mfin','elemSymb','elemZ','elemA','Yield',
+                columns=['mass_ini','Z_ini','mass_fin','elemSymb','elemZ','elemA','Yield',
                 'Mi_windloss','Mi_ini','Xi_avg','Xi_ini','ProdFact']):
     '''
     Example of how to merge multiple yield tables
