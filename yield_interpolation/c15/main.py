a8b18769e32672eb18acd49f2d08c8d9756cc744
Eda Gjergo <eda.gjergo@gmail.com>
2023-09-28 11:21:26 +0800

    add back yield interpolation

---
+++ yield_interpolation/c15/main.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import os
+from yield_interpolation.fit_isotope_interpolants import fit_isotope_interpolants
+
+def parse_c15_raw(_path='galcem/input/yields/lims/c15/tot/'):
+    yield_eps = 1e-13
+    
+    c15_phys = pd.read_fwf(_path+'All_All_All_0_last_tp_20221214_21933.txt')
+    txts = os.listdir(_path+'data/')
+    df = pd.DataFrame({col:[] for col in ['Isotope','A','Z','YIELD','MASS_INI','METALLICITY','IRV','MASS_EJ','MASS_FRACTION']})
+    for txt_og in txts:
+        print(txt_og)
+        if '.DS_Store' in txt_og: continue
+        df_txt = pd.read_fwf(_path+'data/'+txt_og, infer_nrows=400)
+        txt = txt_og.replace('zsun','z1.4m2').replace('_20210617_33100.txt','').replace('yields_tot_m','')
+        prts = txt.split('_')
+        prts = prts[0].split('z')+[prts[1]]
+        mass = float(prts[0].replace('p','.'))
+        metallicity = float(prts[1].replace('z','').replace('m','e-'))
+        irv = int(prts[2])
+        c15_phys_idx = np.where(np.isclose(c15_phys['MASS'],mass) & 
+                       np.isclose(c15_phys['IRV'],irv) & 
+                       np.isclose(c15_phys['METALLICITY'],metallicity))
+        if metallicity in [2e-5,5e-5,1e-4,3e-4]: metallicity = metallicity*2.4
+        df_txt['MASS_INI'] = mass
+        df_txt['METALLICITY'] = metallicity
+        df_txt['IRV'] = irv
+        df_txt['MASS_EJ'] = float(mass - c15_phys['M_H'].iloc[c15_phys_idx])
+        df_txt['MASS_FRACTION'] = df_txt['YIELD']/df_txt['MASS_EJ']
+        df_txt.loc[np.isclose(df_txt['MASS_EJ'],0),'MASS_FRACTION'] = yield_eps
+        df_txt['YSIGN'] = np.sign(df_txt['YIELD'])
+        df = df.append(df_txt)
+        print(df_txt.head())
+        print('\n'+'~'*75+'\n')
+    #df.columns = ['isotope','a','z','yield','mass','metallicity','irv']
+    #dfy0 = df[df['yield']==0]
+    #print('setting %d rows with yield=0 to %.1e'%(len(dfy0),yield_eps))
+    #df.loc[df['yield']==0,'yield'] = yield_eps
+    print(f'{df.columns=}')
+    df.columns = ['isotope','a','z','yield','mass','metallicity','irv','mass_ej','massfrac','ysign']
+    return df
+
+if __name__ == '__main__':
+    root = os.path.abspath(os.path.dirname(__file__))
+    for dirs in ['models', 'figs']:
+        if not os.path.exists(root+'/'+dirs):
+                os.makedirs(root+'/'+dirs)
+    df = parse_c15_raw()
+    df.to_csv(root+'/data.csv',index=False)
+    df = df[df['irv']==0]
+    fit_isotope_interpolants(
+        df = df,
+        root = root,
+        tf_funs = {
+            'mass':lambda x:np.log10(x), 'mass.prime':lambda x:1/(x*np.log(10)),
+            'metallicity':lambda x:np.log10(x), 'metallicity.prime':lambda x:1/(x*np.log(10)),
+            'massfrac':lambda y:np.log10(y), 'massfrac.prime':lambda y:1/(y*np.log(10)), 'massfrac.inv':lambda y:10**y},
+        fit_names = 'all', # 'all', ['c15_z8.a16.irv0.O16'],
+        plot_names =  '[]', # [], 'all', ['c15_z8.a16.irv0.O16'] 
+        )
---
+++ yield_interpolation/lifetime_mass_metallicity/main.py
@@ -0,0 +1,85 @@
+from yield_interpolation.FriendlyInterpolants.friendly_interpolants import SmootheSpline2D_FI
+import pandas as pd
+import numpy as np
+import dill
+import os
+
+def parse_lifetime_mass_metallicity_raw():
+    # parse input tdata
+    df = pd.read_csv('galcem/input/starlifetime/portinari98table14.dat')
+    df.columns = [name.replace('#M','mass').replace('Z=','') for name in df.columns]
+    df = pd.melt(df,id_vars='mass',value_vars=list(df.columns[1:]),var_name='metallicity',value_name='lifetime_yr')
+    df['lifetime_Gyr'] = df['lifetime_yr']/1e9
+    df.drop('lifetime_yr',axis=1,inplace=True)
+    df['metallicity'] = df['metallicity'].astype(float)
+    return df
+
+def plot_mod(fig):
+    from matplotlib import cm
+    axs = fig.get_axes()
+    s_lifetimes_p98 = pd.read_csv('galcem/input/starlifetime/portinari98table14.dat')
+    s_lifetimes_p98.columns = [name.replace('#M','M').replace('Z=0.','Z') for name in s_lifetimes_p98.columns]
+    colordots = np.array([s_lifetimes_p98[c].to_numpy() for c in s_lifetimes_p98.columns[1:]]).flatten()
+    x0label,x1label,ylabel = axs[11].get_xlabel(),axs[11].get_ylabel(),axs[11].get_zlabel()
+    x0label,x1label,ylabel = (lab.replace('$\\mathrm{','').replace('}$','') for lab in [x0label,x1label,ylabel])
+    axs[9].scatter(df[x0label],df[x1label],c=colordots, cmap=cm.get_cmap('Paired'))
+    axs[11].scatter(df[x0label],df[x1label],df[ylabel],c=colordots, cmap=cm.get_cmap('Paired'))
+
+def fit_lifetime_mass_metallicity_interpolants(df,root):
+    # lifetime by mass, metallicity
+    #   fit model
+    lifetime_by_mass_metallicity = SmootheSpline2D_FI(
+        df = df,
+        ycol = 'lifetime_Gyr',
+        tf_funs = {
+            'mass':lambda x:np.log10(x), 'mass.prime':lambda x:1/(x*np.log(10)),
+            'metallicity':lambda x:np.sqrt(x), 'metallicity.prime':lambda x:1/(2*np.sqrt(x)),
+            'lifetime_Gyr':lambda y:np.log10(y), 'lifetime_Gyr.prime':lambda y:1/(y*np.log(10)), 'lifetime_Gyr.inv':lambda y:10**y},
+        name = 'LifetimeInterpolant',
+        plot = [None,'mass','metallicity'],#False,
+        fig_root = root+'/figs/',
+        plot_ops={'scatter':False,'sepfl':0,'sepfr':0,'sepfb':0,'sepft':0},
+        plot_mod = plot_mod)
+    #   print model
+    print(lifetime_by_mass_metallicity)
+    #   save model
+    dill.dump(lifetime_by_mass_metallicity,open(root+'/models/lifetime_by_mass_metallicity.pkl','wb'))
+    #   load model
+    lifetime_by_mass_metallicity_loaded = dill.load(open(root+'/models/lifetime_by_mass_metallicity.pkl','rb'))
+    #   example model use
+    yquery = lifetime_by_mass_metallicity_loaded(df)
+    dyquery_dmass = lifetime_by_mass_metallicity_loaded(df,dwrt='mass')
+    dyquery_dmetallicity = lifetime_by_mass_metallicity_loaded(df,dwrt='metallicity')
+    # mass by lifetime, metallicity
+    mass_by_lifetime_metallicity = SmootheSpline2D_FI(
+        df = df[['lifetime_Gyr','mass','metallicity']],
+        ycol = 'mass',
+        tf_funs = {
+            'lifetime_Gyr':lambda x:np.log10(x), 'lifetime_Gyr.prime':lambda x:1/(x*np.log(10)),
+            'metallicity':lambda x:np.sqrt(x), 'metallicity.prime':lambda x:1/(2*np.sqrt(x)),
+            'mass':lambda y:np.log10(y), 'mass.prime':lambda y:1/(y*np.log(10)), 'mass.inv':lambda y:10**y},
+        name = 'MassInterpolant',
+        plot = [None,'lifetime_Gyr','metallicity'], #False,
+        fig_root = root+'/figs/',
+        plot_ops={'scatter':False,'sepfl':0,'sepfr':0,'sepfb':0,'sepft':0},
+        plot_mod = plot_mod)
+    #       print model
+    print(mass_by_lifetime_metallicity)
+    #       save model
+    dill.dump(mass_by_lifetime_metallicity,open(root+'/models/mass_by_lifetime_metallicity.pkl','wb'))
+    #       load model
+    mass_by_lifetime_metallicity_loaded = dill.load(open(root+'/models/mass_by_lifetime_metallicity.pkl','rb'))
+    #       example model use
+    yquery = mass_by_lifetime_metallicity_loaded(df)
+    dyquery_dlifetime = mass_by_lifetime_metallicity_loaded(df,dwrt='lifetime_Gyr')
+    dyquery_dmetallicity = mass_by_lifetime_metallicity_loaded(df,dwrt='metallicity')
+
+if __name__ == '__main__':
+    root = os.path.abspath(os.path.dirname(__file__))    
+    for dirs in ['models', 'figs']:
+        if not os.path.exists(root+'/'+dirs):
+                os.makedirs(root+'/'+dirs)
+    df = parse_lifetime_mass_metallicity_raw()
+    df.to_csv(root+'/data.csv',index=False)
+    fit_lifetime_mass_metallicity_interpolants(df,root)
+    
